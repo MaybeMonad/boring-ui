aa51a726e329a47b71252f4fa4ca1cc867824a04
MaybeMonad <dezineleo@gmail.com>
2024-11-07 00:59:46 +0800

    feat: add switch and checkbox

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,6 +2,8 @@
 import { ComponentCard } from "@/components/component-card";
 import { SiteHeader } from "@/components/site-header";
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Switch } from "@/components/ui/switch";
 
 export default function Home() {
 	return (
@@ -11,9 +13,12 @@ export default function Home() {
 				<ComponentCard name="Button">
 					<Button>Click me</Button>
 				</ComponentCard>
-				{/* <ComponentCard name="Button">
-					<Button>Click me</Button>
-				</ComponentCard> */}
+				<ComponentCard name="Switch">
+					<Switch />
+				</ComponentCard>
+				<ComponentCard name="Checkbox">
+					<Checkbox />
+				</ComponentCard>
 			</div>
 			{/* <ThemeToggle /> */}
 		</div>
